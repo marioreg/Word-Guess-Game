c0e3ad1f61139092ecc0e3affaa6e2b5462a96e0
marioreg <marioreg@hotmail.com>
2018-05-07 20:35:00 -0500

    I think I got it

--- javascript/game.js
+++ javascript/game.js
@@ -1,74 +1,151 @@
 //DOM elements
-var DstartButton = document.getElementById("start-button");
-var DplaceHolder = document.getElementById("placeholder");
-var DguessedSofar = document.getElementById("guessed_sofar");
-var DguessesLeft = document.getElementById("guesses_left");
-var Dwins = document.getElementById("correct_wins");
-var Dlosses = document.getElementById("wrong_losses");
+var $newGameButton = document.getElementById("start-button");
+var $placeholder = document.getElementById("placeholder");
+var $guessedLetters = document.getElementById("guessed_sofar");
+var $guessesLeft = document.getElementById("guesses_left");
+var $wins = document.getElementById("correct_wins");
+var $losses = document.getElementById("wrong_losses");
 
 //Variables
 
-var words = ["nirvana", "metallica", "pearl Jam", "black sabbath", "primus", "garbage", "foo fighters"];
+var wordBank = ["nirvana", "metallica", "pearl Jam", "black sabbath", "primus", "garbage", "foo fighters", "guns n roses"];
 
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
-var gameRuns = false;
+var gameRunning = false;
 var pickedWord ='';
-var pickedWordPlaceholder = [];
-var guessedLetterArray = [];
-var wrongLetterArray = [];
+var pickedWordPlaceholderArr = [];
+var guessedLetterBank = [];
+var incorrectLetterBank = [];
 
 
 //game function
 
 //reset all game info
 function newGame() {
-
-    gameRuns = true;
-    guessesLeft = 10;
-    guessedLetterArray = [];
-    wrongLetterArray = [];
-    pickedWordPlaceholder = [];
-
-    //pick a new word
-    pickedWord = words[Math.floor(Math.random() * words.length)];
-
-    //create place holders out of new word
-    for (var i =  0; i < pickedWord.length; i++){
-
-            if (pickedWord[i] === ' ') {
-
-                    pickedWordPlaceholder.push(' ');
-                } else {
-
-                    pickedWordPlaceholder.push('_');
-                    }
-            }
-
-//write it all to the DOM
-
-DguessesLeft.textContent = guessesLeft;
-DplaceHolder.textContent = pickedWordPlaceholder.join('');
-DguessedSofar.textContent = wrongLetterArray;
-
+	
+	gameRunning = true;
+	guessesLeft = 10;
+	guessedLetterBank = [];
+	incorrectLetterBank = [];
+	pickedWordPlaceholderArr = [];
+    
+	
+	//pick a new wordBank
+	pickedWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+	
+	//create placeholders out of new length
+	
+	for (var i=0; i < pickedWord.length; i++){
+		
+		if(pickedWord[i]== " "){
+			
+			pickedWordPlaceholderArr.push(" ");
+		} else {
+			pickedWordPlaceholderArr.push("_");
+		}
+	}
+	
+	//Write it all to the DOM
+	$guessesLeft.textContent = guessesLeft;
+	$placeholder.textContent = pickedWordPlaceholderArr.join("");
+	$guessedLetters.textContent = incorrectLetterBank;
 }
 
 
 //letters guessed function
-
+function leterGuess(letter) {
+	
+	
+	
+	if (gameRunning === true && guessedLetterBank.indexOf(letter) === -1){
+		//run Game logic
+		guessedLetterBank.push(letter);
+		
+		//check if guessed letter is in picked wordBank
+		
+		for (var i = 0; i < pickedWord.length; i++) {
+			//convert both values to lower case for comparison
+			if (pickedWord[i].toLowerCase() === letter.toLowerCase()){
+				//if a match, replace character in placeholder for the letter
+				pickedWordPlaceholderArr[i] = pickedWord[i];
+			}
+		}
+		
+		$placeholder.textContent = pickedWordPlaceholderArr.join("");
+		checkIncorrect(letter);
+	}
+	
+	else {
+		if (gameRunning === false) {
+			alert ("The game is not running")
+		} else {
+			alert ("You already guessed this letter! Try again.");
+		}
+	}
+	
+}
 
 
 //check for incorrect letter
 
+	function checkIncorrect(letter){
+		//Check to see if letter did not make it into the placeholder / incorrect guessed
+		if
+		(pickedWordPlaceholderArr.indexOf(letter.toLowerCase()) === -1 && pickedWordPlaceholderArr.indexOf(letter.toUpperCase()) === -1) {
+			//Decrement guesses
+			guessesLeft--;
+			//Add incorrect letter to incorrectLetterBank
+			incorrectLetterBank.push(letter);
+			//Write new bank of incorrect letters guessed to DOM
+			$guessedLetters.textContent = incorrectLetterBank.join(" ");
+			//Write new amount of guesses left to DOM
+			$guessesLeft.textContent = guessesLeft;			
+		}
+		checkLoss();
+		
+	}
+
 
 //check losses
+
+function checkLoss(){
+	
+	if (guessesLeft === 0){
+		losses++;
+		gameRunning = false;
+		$losses.textContent = losses;
+		$placeholder.textContent = pickedWord;
+		
+	}
+	checkWin();
+	
+}
 
 
 //check wins
 
+function checkWin(){
+	if (pickedWord.toLowerCase() === pickedWordPlaceholderArr.join("").toLowerCase())
+	{
+		wins++;
+		gameRunning = false;
+		$wins.textContent = wins;
+	}
+	
+}
+
 
 //event listener for button
-DstartButton.addEventListener("click", newGame);
+$newGameButton.addEventListener("click", newGame);
 
 //add onkeyup event to trigger letterguess
+
+document.onkeyup = function(event){
+	if (event.keyCode >= 65 && event.keyCode <= 90){
+		
+		leterGuess(event.key);
+		
+	}
+}
